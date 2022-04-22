b48d585f65d304e9aac8c5fca1e8d4c4ed03b7aa
bogu <boguxdeus@protonmail.com>
2022-04-22 14:39:11 +0200

    Nastawienie potworów bagno
    Zmiana nastawienia na neutralne dla bagiennych szczurów, szkieletów, zombie, aby się nie biły na bagnie.

--- Content/AI/Monster/B_Monster/B_InitMonsterAttitudes.d
+++ Content/AI/Monster/B_Monster/B_InitMonsterAttitudes.d
@@ -778,7 +778,7 @@ func void B_InitMonsterAttitudes ()
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_HOSTILE,	GIL_TROLL					);	// Troll / Schwarzer Troll
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_NEUTRAL,	GIL_SWAMPSHARK				);
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_FRIENDLY,	GIL_DRAGON					);	// Feuerdrache / Eisdrache / Felsdrache / Sumpfdrache / Untoter Drache
-	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_HOSTILE,	GIL_MOLERAT					);	
+	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_NEUTRAL,	GIL_MOLERAT					);	
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_NEUTRAL,	GIL_ORC						);	// Ork-Krieger / Ork-Shamane / Ork-Elite
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_NEUTRAL,	GIL_FRIENDLY_ORC			);	
 	B_SetMonsterAttitude	(GIL_SKELETON,	ATT_FRIENDLY,	GIL_UNDEADORC				);	// Ork-Sklave / Ur-Shak	
@@ -915,9 +915,9 @@ func void B_InitMonsterAttitudes ()
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_FRIENDLY,	GIL_DEMON					);
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_HOSTILE,	GIL_SUMMONED_DEMON			);
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_HOSTILE,	GIL_TROLL					);	// Troll / Schwarzer Troll
-	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_HOSTILE,	GIL_SWAMPSHARK				);
+	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_NEUTRAL,	GIL_SWAMPSHARK				);
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_FRIENDLY,	GIL_DRAGON					);	// Feuerdrache / Eisdrache / Felsdrache / Sumpfdrache / Untoter Drache
-	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_HOSTILE,	GIL_MOLERAT					);	
+	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_NEUTRAL,	GIL_MOLERAT					);	
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_NEUTRAL,	GIL_ORC						);	// Ork-Krieger / Ork-Shamane / Ork-Elite
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_NEUTRAL,	GIL_FRIENDLY_ORC			);	
 	B_SetMonsterAttitude	(GIL_ZOMBIE,	ATT_FRIENDLY,	GIL_UNDEADORC				);	// Ork-Sklave / Ur-Shak	
